27723b8196b68ed723966adc3c9c7fc1b1def85f
MircoGruppi <mircogruppi.gruppi@gmail.com>
2026-04-30 15:01:25 +0200

    Create numero.R

---
+++ R/numero.R
@@ -0,0 +1,10 @@
+numero <- function (x){
+    if(x>0){
+        "Questo numero è positivo"
+        }
+    else if(x<0) {
+        "Questo numero è negativo"}
+    else {
+        "Zero non è negativo né positivo"
+        }
+}
